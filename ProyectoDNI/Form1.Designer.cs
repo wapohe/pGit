2f3bb3861afcc4a53601d9be694f796626a784cd
Victor <victorpergom@monlau.com>
2020-02-05 16:40:46 +0100

    VERSION 0.0.1.

---
+++ ProyectoDNI/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ProyectoDNI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxChar = new System.Windows.Forms.TextBox();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Location = new System.Drawing.Point(142, 91);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(192, 20);
+            this.textBoxDNI.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DNI";
+            // 
+            // textBoxChar
+            // 
+            this.textBoxChar.Location = new System.Drawing.Point(373, 91);
+            this.textBoxChar.Name = "textBoxChar";
+            this.textBoxChar.Size = new System.Drawing.Size(56, 20);
+            this.textBoxChar.TabIndex = 2;
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(226, 188);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(108, 50);
+            this.buttonAceptar.TabIndex = 3;
+            this.buttonAceptar.Text = "ACEPTAR";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            this.buttonAceptar.Click += new System.EventHandler(this.buttonAceptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.textBoxChar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDNI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxChar;
+        private System.Windows.Forms.Button buttonAceptar;
+    }
+}
+
---
+++ ProyectoDNI/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoDNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            int dni = Convert.ToInt32(textBoxDNI.Text);
+            char letter = functionLetter(dni);
+            textBoxChar.Text = letter.ToString();
+        }
+
+        private char functionLetter(int dni)
+        {
+            char letterDNI='a';
+
+            return letterDNI;
+        }
+    }
+}
